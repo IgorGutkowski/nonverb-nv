cebe408bcbc44732490caf4cbfba8b60b40f7d42
IgorGutkowski <91725295+IgorGutkowski@users.noreply.github.com>
2023-12-05 08:56:32 +0100

    dodanie kamery

--- flask-app/my-react-app/my-react-app/src/App.js
+++ flask-app/my-react-app/my-react-app/src/App.js
@@ -1,21 +1,87 @@
 // src/App.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './App.css';
 
 function App() {
   const [message, setMessage] = useState('');
+  const [photo, setPhoto] = useState('');
+  const [videoStream, setVideoStream] = useState(null);
+  const [hasCamera, setHasCamera] = useState(true);
 
   useEffect(() => {
     // Make a request to your Flask backend
     axios.get('http://localhost:5000/')
       .then(response => setMessage(response.data.message))
       .catch(error => console.error('Error fetching data:', error));
+
+    // Start video stream
+    startVideo();
   }, []);
+
+  const startVideo = () => {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+      .then(stream => {
+        setVideoStream(stream);
+        const videoElement = document.getElementById('video');
+        videoElement.srcObject = stream;
+      })
+      .catch(error => {
+        console.error('Error accessing webcam:', error);
+        setHasCamera(false);
+      });
+  };
+
+  const takePhoto = () => {
+    const videoElement = document.getElementById('video');
+    const canvas = document.createElement('canvas');
+    canvas.width = videoElement.width;
+    canvas.height = videoElement.height;
+
+    const context = canvas.getContext('2d');
+    context.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
+
+    const dataUrl = canvas.toDataURL('image/png');
+    setPhoto(dataUrl);
+
+    // Send the captured photo to Flask backend
+    sendPhotoToBackend(dataUrl);
+  };
+
+  const sendPhotoToBackend = async (photoData) => {
+    try {
+      const response = await axios.post('http://localhost:5000/classify_emotion', {
+        photo: photoData,
+      });
+      // Assuming the Flask backend sends back the emotion classification result
+      alert(`Emotion: ${response.data.emotion}`);
+    } catch (error) {
+      console.error('Error sending photo to backend:', error);
+    }
+  };
 
   return (
     <div className="App">
       <h1>Take photo</h1>
-      <h1>{message}</h1>
+      {hasCamera ? (
+        <div>
+          <video id="video" width="320" height="240" autoPlay></video>
+          <div>
+            <button onClick={takePhoto}>Take Photo</button>
+          </div>
+          {photo && (
+            <div>
+              <h2>Captured Photo:</h2>
+              <img src={photo} alt="Captured" width="320" height="240" />
+            </div>
+          )}
+        </div>
+      ) : (
+        <div>
+          <p>No camera detected on this device.</p>
+          <p>{message}</p>
+        </div>
+      )}
     </div>
   );
 }
--- flask-app/server/app.py
+++ flask-app/server/app.py
@@ -1,13 +1,40 @@
 # app.py
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+import base64
+from PIL import Image
+import io
 
 app = Flask(__name__)
 CORS(app)
 
+# Replace the following placeholder function with your actual emotion classification logic
+def classify_emotion(photo_data):
+    # Placeholder code (replace with your emotion classification model)
+    # Assuming you have a function like classify_emotion_model(photo_data)
+    # that returns the predicted emotion
+    return classify_emotion_model(photo_data)
+
 @app.route('/')
 def hello_world():
     return jsonify(message="Nonverb!")
+
+@app.route('/classify_emotion', methods=['POST'])
+def classify_emotion_route():
+    try:
+        data = request.get_json()
+        photo_data = data.get('photo')
+
+        # Convert base64 image data to PIL Image
+        image = Image.open(io.BytesIO(base64.b64decode(photo_data.split(',')[1])))
+
+        # Perform emotion classification
+        emotion = classify_emotion(image)
+
+        return jsonify({'emotion': emotion})
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return jsonify({'error': 'Internal Server Error'}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
